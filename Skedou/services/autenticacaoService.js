7b8892801061fc113cdf105031e9e3985cfb7c1b
Thiago Soares <thiago.soares.seraos@gmail.com>
2025-11-02 21:42:23 -0300

    feat: adicionando apis de login e cadastro

--- Skedou/services/autenticacaoService.js
+++ Skedou/services/autenticacaoService.js
@@ -1,12 +1,8 @@
-import axios from "axios";//biblioteca q faz auth http
-
-const api = axios.create({
-  baseURL: "http://localhost:3000/api", 
-});
+import api from "./api";
 
 export const cadastrarUsuario = async (dados) => {
   try {
-    const resposta = await api.post("??", dados);
+    const resposta = await api.post("/api/usuarios/cadastro", dados);
     return resposta.data;
   } catch (erro) {
     console.error("Erro ao cadastrar:", erro);
@@ -16,10 +12,20 @@ export const cadastrarUsuario = async (dados) => {
 
 export const logarUsuario = async (dados) => {
   try {
-    const resposta = await api.post("??", dados);
+    const resposta = await api.post("/api/usuarios/login", dados);
     return resposta.data;
   } catch (erro) {
     console.error("Erro ao logar:", erro);
+    throw erro;
+  }
+};
+
+export const obterPerfilUsuario = async () => {
+  try {
+    const resposta = await api.get("/api/usuarios/perfil");
+    return resposta.data;
+  } catch (erro) {
+    console.error("Erro ao obter perfil:", erro);
     throw erro;
   }
 };
